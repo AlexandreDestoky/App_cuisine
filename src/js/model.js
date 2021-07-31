54cb4c6df0873350380ca50a431ce091e05f9961
Destoky <adestoky@gmail.com>
2021-07-31 12:21:24 +0200

    287. Helpers and Configuration Files

--- src/js/model.js
+++ src/js/model.js
@@ -1,9 +1,27 @@
+import { async } from "regenerator-runtime";
+import { API_URL } from "./config.js";
+import { getJSON } from "./helper.js";
+
 export const state = {
-  recipe: {
+  recipe: {},
+};
 
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}/${id}`);
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (error) {
+    alert(error);
   }
-}
-
-export const loadRecipe = async function() {
-  
-}
+};
